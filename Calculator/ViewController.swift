dff06f03fcecd3b02125305204324fa5dd4b9f6c
Alexander Kompch <alexander.kompch@web.de>
2015-11-01 18:25:04 +0100

    implemented sqrt as math operation using only one argument

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -35,13 +35,23 @@ class ViewController: UIViewController
             case "÷": performOperation { $1 / $0 }
             case "+": performOperation { $0 + $1 }
             case "−": performOperation { $1 - $0 }
+            case "√": performOperation { sqrt($0) }
         default: break
         }
     }
     
+    // performOperation for two argument case
     func performOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+   
+    // performOperation for one argument case
+    func performOperation(operation: (Double) -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
             enter()
         }
     }
