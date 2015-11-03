7664975f0ce61cc017ce74e10f8157c856898191
Alexander Kompch <alexander.kompch@web.de>
2015-11-03 19:55:27 +0100

    separated Model (in MVC) as CalculatorBrain from ViewController (Controller in MVC)

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,74 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Alexander Kompch on 03.11.15.
+//  Copyright (c) 2015 Alexander Kompch. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    enum Op {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+    
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String:Op]()
+    
+    init() {
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+") { $0 + $1 }
+        knownOps["−"] = Op.BinaryOperation("−", +)
+        knownOps["√"] = Op.UnaryOperation("√") { sqrt($0) }
+    }
+    
+    private func evaluate(ops: [Op]) -> (result:Double?, remainingOps:[Op]) {
+        if !ops.isEmpty {
+            var remaingOps = ops
+            let op = remaingOps.removeLast()
+            switch op {
+            case Op.Operand(let operand):
+                return (operand, remaingOps)
+            case Op.UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remaingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case Op.BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remaingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        return result
+    }
+    
+    func pushOperand (operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+    
+}
